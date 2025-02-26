b1e71a091840286854d25f6511b15304fd1b1bd5
Deepak-647 <dbehera647@gmail.com>
2025-02-26 16:09:39 +0530

    Created userSignup and captainSignup form

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-      <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1572848119280-46bca6fabb75?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex flex-col justify-between w-full bg-red-400'>
+      <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1572848119280-46bca6fabb75?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex flex-col justify-between w-full'>
         <img className='w-14 ml-9 ' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png" alt="" />
         <div className='bg-white pb-7 py-4 px-4'>
           <h2 className='text-2xl font-bold'>Get Started with Uber</h2>
